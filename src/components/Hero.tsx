9fe3c63fcfeea4bb0296fa56526e38d2b547b09d
tingu mingu <tingumingutoys@gmail.com>
2026-07-20 02:30:54 +0500

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,43 @@
+import hero from "../assets/images/hero.jpg";
+
 function Hero() {
   return (
     <section className="hero">
-      <div className="hero-text">
-        <h1>Best Toys For Happy Kids</h1>
+
+      <div className="hero-content">
+
+        <span className="tag">
+          ⭐ New Collection 2026
+        </span>
+
+        <h1>
+          Discover Amazing <br />
+          Toys For Every Child
+        </h1>
+
         <p>
-          Discover amazing toys, games and gifts for your little ones.
+          Premium quality toys, fashion and accessories for
+          Men, Women, Kids and Families.
         </p>
 
-        <button>
-          Shop Now
-        </button>
+        <div className="hero-buttons">
+          <button className="shopBtn">
+            Shop Now
+          </button>
+
+          <button className="exploreBtn">
+            Explore
+          </button>
+        </div>
+
       </div>
 
       <div className="hero-image">
-        🧸
+        <img src={hero} alt="Hero" />
       </div>
+
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
